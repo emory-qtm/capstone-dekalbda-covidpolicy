742de910e0c04b48d043887d01a1fbf514110e66
FREEDDDDD <xfy3272251635@gmail.com>
2023-02-03 15:21:15 -0500

    The preliminary merging

--- Merging.py
+++ Merging.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 
 Prosecutor_Data = pd.read_csv('Data/Prosecutor Data-01-25-2023.csv')
 Sentencing_Data = pd.read_csv('Data/Sentencing Information-01-25-2023.csv')
@@ -10,6 +9,20 @@ Prosecutor_Data['Matching_ID'] = Prosecutor_Data.CaseNumber + Prosecutor_Data.Un
 
 Merged = Sentencing_Data.merge(Prosecutor_Data, on='Matching_ID', how='left')
 Merged.to_csv("test.csv")
+
+col_values = []
+for col in Merged:
+    col_values.append(Merged[col].unique())
+
+col_names = []
+for col in Merged.columns:
+    col_names.append(col)
+
+uniqe_value = {col_names[i]: col_values[i] for i in range(len(col_names))}
+
+with open("unqie_value.txt", 'w') as f:
+    for key, value in uniqe_value.items():
+        f.write('%s:%s\n' % (key, value))
 
 
 
